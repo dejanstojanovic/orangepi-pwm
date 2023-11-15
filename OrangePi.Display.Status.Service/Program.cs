baee2342da678e3ed8c8e68fd35893fffbf1cdf4
Dejan Stojanovic <d.stojanovic@hotmail.com>
2023-11-15 13:14:42 +0400

    feat: adds simple text display

---
+++ OrangePi.Display.Status.Service/Program.cs
@@ -0,0 +1,10 @@
+using OrangePi.Display.Status.Service;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
