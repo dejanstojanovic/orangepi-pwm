5ce097d79369f6d2a09afdbbebad2e9486278fed
Dejan Stojanovic <d.stojanovic@hotmail.com>
2023-11-15 14:25:20 +0400

    feat: adds display writing

--- OrangePi.Display.Status.Service/Program.cs
+++ OrangePi.Display.Status.Service/Program.cs
@@ -3,7 +3,7 @@ using OrangePi.Display.Status.Service;
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        services.AddHostedService<Worker>();
+        services.AddHostedService<App>();
     })
     .Build();
 
