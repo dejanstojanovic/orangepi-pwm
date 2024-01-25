144aa5a9cb7c56919e69e03af5e918be01b8a07e
Dejan Stojanovic <d.stojanovic@hotmail.com>
2024-01-25 21:55:55 +0400

    feat: adds tachometer test

--- MotionSensorSample/Program.cs
+++ MotionSensorSample/Program.cs
@@ -1,17 +1,56 @@
-﻿using System.Device.Gpio;
+﻿using System.CodeDom;
+using System.Device.Gpio;
 using System.Device.Pwm.Drivers;
 
 using var controller = new GpioController();
-var pin = controller.OpenPin(92, PinMode.Input);
+var pin = controller.OpenPin(52, PinMode.Input);
+const int fins = 7;
+var hits = new List<bool>();
+var locker = new Object();
+
+System.Timers.Timer timer = new System.Timers.Timer(TimeSpan.FromSeconds(1));
+timer.Elapsed += timer_Elapsed;
+
+void timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+{
+    lock (locker)
+    {
+        var result = hits.Where((e, i) => i < hits.Count - 1)
+                .Select((e, i) => new { A = e, B = hits[i + 1] })
+                .Where((c, i) => i % 2 == 0)
+                .Where(e => e.A != e.B)
+                .Count();
+
+        hits.Clear();
+
+        Console.Clear();
+        Console.WriteLine($"rpm: {(result * 60)}");
+
+        timer.Stop();
+        timer.Start();
+    }
+
+}
+
+timer.Start();
 while (true)
 {
     var value = pin.Read();
-    
+
     if (value == PinValue.High)
     {
-        Console.WriteLine("DETECTED!");
-
+        lock (locker)
+        {
+            hits.Add(true);
+        }
+    }
+    else
+    {
+        lock (locker)
+        {
+            hits.Add(false);
+        }
     }
 
-    await Task.Delay(TimeSpan.FromMilliseconds(200));
+    await Task.Delay(TimeSpan.FromMilliseconds(10));
 }
