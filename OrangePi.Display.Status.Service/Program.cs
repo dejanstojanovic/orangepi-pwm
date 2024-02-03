307e5059ad8e38da133b40d6c5ef0bd90bd105ba
Dejan Stojanovic <d.stojanovic@hotmail.com>
2024-02-03 21:25:59 +0400

    feat: updates DI

--- OrangePi.Display.Status.Service/Program.cs
+++ OrangePi.Display.Status.Service/Program.cs
@@ -34,7 +34,8 @@ IHost host = Host.CreateDefaultBuilder(args)
         services.AddTransient<IInfoService>(x => new SsdInfoService(
            processRunner: x.GetRequiredService<IProcessRunner>(),
            temperatureReaders: x.GetRequiredService<IEnumerable<ITemperatureReader>>(),
-           driveMount: "/dev/nvme0n1p2"));
+           driveMount: "/dev/nvme0n1p2",
+           logger: x.GetRequiredService<ILogger<SsdInfoService>>()));
 
         services.AddTransient<IHostInfoService>(x => new HostInfoService(
            processRunner: x.GetRequiredService<IProcessRunner>(),
