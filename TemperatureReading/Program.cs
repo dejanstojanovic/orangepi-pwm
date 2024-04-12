a3a8bb04f921dd070bb87ca26d7657d4b61add61
Dejan Stojanovic <d.stojanovic@hotmail.com>
2024-04-12 22:03:33 +0400

    feat: adds DHT11 sample

---
+++ TemperatureReading/Program.cs
@@ -0,0 +1,33 @@
+﻿// GPIO Pin
+using Iot.Device.Common;
+using Iot.Device.DHTxx;
+using UnitsNet;
+
+using (Dht11 dht = new Dht11(257))
+{
+    while (true)
+    {
+
+
+        Temperature temperature = default;
+        RelativeHumidity humidity = default;
+        bool success = dht.TryReadHumidity(out humidity); //&& dht.TryReadTemperature(out temperature);
+        // You can only display temperature and humidity if the read is successful otherwise, this will raise an exception as
+        // both temperature and humidity are NAN
+        if (success)
+        {
+            Console.WriteLine($"Temperature: {temperature.DegreesCelsius:F1}\u00B0C, Relative humidity: {humidity.Percent:F1}%");
+
+            // WeatherHelper supports more calculations, such as saturated vapor pressure, actual vapor pressure and absolute humidity.
+            Console.WriteLine(
+                $"Heat index: {WeatherHelper.CalculateHeatIndex(temperature, humidity).DegreesCelsius:F1}\u00B0C");
+            Console.WriteLine(
+                $"Dew point: {WeatherHelper.CalculateDewPoint(temperature, humidity).DegreesCelsius:F1}\u00B0C");
+        }
+        else
+        {
+            Console.WriteLine("Error reading DHT sensor");
+        }
+        await Task.Delay(TimeSpan.FromSeconds(1));
+    }
+}
